4f2fca8bde62abf321350f6fbb0d3d1d7dd75e40
Casey Chow <digitxpc@gmail.com>
2014-06-11 13:38:13 -0400

    add Track Height constant

--- projustor.ino
+++ projustor.ino
@@ -10,6 +10,7 @@ const int SERVOPIN = 7; // pin corresponding to servo
 
 const long FOCALLENGTH; // TODO: Find focal length.
 const long LENSPOSITION; // Defined in terms of distance from sensor.
+const long TRACKHEIGHT; // The height between the track and the servo.
 long objectDistance;
 
 Ultrasonic ultrasonic(ULTRASONICPIN);
@@ -30,7 +31,7 @@ void loop() {
   tgtPosition = idealPosition(range, FOCALLENGTH, LENSPOSITON);
   
   // 3. Move the object to the desired position.
-  servo.MoveObjToPos(tgtPosition);
+  servo.MoveObjToPos(tgtPosition, TRACKHEIGHT);
   delay(100);
 }
 
