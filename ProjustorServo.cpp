dd1db19b8de7d5430033615a0e6dc8baf34c39a0
Casey Chow <digitxpc@gmail.com>
2014-06-11 13:21:43 -0400

    implement ProjustorServo class

--- ProjustorServo.cpp
+++ ProjustorServo.cpp
@@ -1,29 +1,19 @@
 #include "ProjustorServo.h"
+#include <math.h>
 #include <Servo.h>
 
 ProjustorServo::ProjustorServo(int pin)
 {
    _pin = pin;
+   servo.attach(pin);
 }
-/*Begin the detection and get the pulse back signal*/
-void ProjustorServo::MoveObjToPos(long newPosition) {
-  
+
+void ProjustorServo::MoveObjToPos(long newPosition, long height) {
+  // https://casry.hackpad.com/Servo-Calculations-15QDaQiFuMh
+  int angle = (int) atan((long) newPosition / height) + 90;
+  MoveObjToAng(angle);
 }
 
 void ProjustorServo::MoveObjToAng(int newAng) {
-  int pulse = pulseLength(newPosition);
-  
-  pinMode(_pin, OUTPUT);
-  digitalWrite(_pin, HIGH);
-  delayMicroseconds(pulse);
-  digitalWrite(_pin,LOW);
-}
-
-
-
-int ProjustorServo::pulseLength(long tgtPosition) {
-  // 5V for 500 microseconds = 0.5 milliseconds and corresponds to 0 degrees
-  // 5V for 1500 microseconds = 1.5 milliseconds and corresponds to 90 degrees
-  // 5V for 2500 microseconds = 2.5 milliseconds and corresponds to 180 degrees
-  
+  servo.write(newAng);
 }
