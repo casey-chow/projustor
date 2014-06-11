075b2e70557227207a949c33947e2bc315e64ab5
Casey Chow <digitxpc@gmail.com>
2014-06-11 13:30:31 -0400

    Remove objPosition method

--- ProjustorServo.h
+++ ProjustorServo.h
@@ -6,7 +6,7 @@ class ProjustorServo
 		ProjustorServo(int pin);
                 void MoveObjToPos(long newPosition, long height);
                 void MoveObjToAng(int newAng);
-		long objPosition(void);
+                
 	private:
 		int _pin; //pin number of Arduino that is connected with SIG pin of servo
                 
