56ae2457117e1126fca534943605cd0d4bdbf740
Casey Chow <digitxpc@gmail.com>
2014-06-11 13:09:58 -0400

    Add servo object to projustorservo

--- ProjustorServo.h
+++ ProjustorServo.h
@@ -1,3 +1,5 @@
+#include <Servo.h>
+
 class ProjustorServo
 {
 	public:
@@ -8,5 +10,5 @@ class ProjustorServo
 		int _pin; //pin number of Arduino that is connected with SIG pin of servo
                 long _position; // position of the servo
                 
-                int pulseLength(long tgtPosition); // get pulse length 
+                Servo servo;
 };
