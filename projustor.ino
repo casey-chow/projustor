67eab284f8c781cd53b546585a4c2a37830aab08
Casey Chow <digitxpc@gmail.com>
2014-06-17 13:08:21 -0400

    Finalize code; calibrate

--- projustor.ino
+++ projustor.ino
@@ -11,45 +11,61 @@ const int ULTRASONICPIN = 22; // pin corresponding to the ultrasonic sensor
 const int SERVOPIN = 7; // pin corresponding to servo
 
 const double FOCALLENGTH = 10.0;
-const double LENSPOSITION = 10.0; // Defined in terms of distance from sensor.
-const double SERVOPOSITION = 5.0; // TODO: Find.
-const double TRACKHEIGHT = 10.0; // The height between the track and the servo.
-long objectDistance;
+const double P_LENS = 7.5; // Defined in terms of distance from sensor.
+const double P_SERVO = -3.0; // TODO: Find.
+const double TRACKHEIGHT = 10.1; // The height between the track and the servo.
+
+double angleLast = 0.0;
 
 Servo servo;
 
 void setup() {
   Serial.begin(9600); // start serial measurement at 9600bps
   servo.attach(SERVOPIN);
+  
+  moveToAngle(90);
 }
 
 void loop() {
-  long pulse;
-  double range;
-  double tgtPosition; // of object
-  int tgtAngle;
+  // Quick note on naming:
+  // p in a variable refers to position. Position is relative to the range sensor.
+  // d in a variable refers to a distance. Distance must have two objects specified.
+  //   Distance is a kind of a misnomer because we're measuring negative values. Whatevs.
   
-  // 1. Get distance from lens to screen.
-  pulse = measureDistance();
-  Serial.print("Pulse Length (ms): ");
-  Serial.print(pulse);
+  // 1a. Get position of screen.
+  double pScreen = pulseToCm(measureDistance());
+  Serial.print("pScreen: ");
+  Serial.print(pScreen);
   
-  range = pulseToCm(pulse) - LENSPOSITION;
-  Serial.print("\t| Screen Position (cm): ");
-  Serial.print(range);
+  // 1b. Get distance from lens to screen.
+  double dLensImage = pScreen - P_LENS;
   
-  // 2. Get desired lens position according to measured screen distance.
-  tgtPosition = idealPosition(range);
-  Serial.print("\t| Target Position (cm): ");
-  Serial.print(tgtPosition);
+  // 2a. Calculate necessary distance from object to lens.
+  double dLensObject = idealDObject(dLensImage);
+  Serial.print("\t| dLensObject (cm): ");
+  Serial.print(dLensObject);
   
-  // 3. Move the object to the desired position.
-  tgtAngle = angleForPos(tgtPosition);
-  Serial.print("\t| Angle: ");
+  // 2b. Calculate necessary position of object.
+  double pObject = P_LENS - dLensObject;
+  
+  // 3b. Calculate distance of object relative to servo.
+  double dServoObject = pObject - P_SERVO;
+  Serial.print("\t|dServoObject (cm): ");
+  Serial.print(dServoObject);
+  
+  // 3b. Move the object to the desired position.
+  int tgtAngle = angleForPos(dServoObject);
+  Serial.print("\t| tgtAngle: ");
   Serial.print(tgtAngle);
   
-  moveToPos(tgtAngle);
-  Serial.println(" | Moved");
+  if (tgtAngle > 55 && tgtAngle < 125 && tgtAngle != angleLast) { 
+    moveToAngle(tgtAngle);
+    Serial.println(" | Moved");
+    angleLast = tgtAngle;
+  } else {
+    Serial.println("");
+  }
+  
   delay(100);
 }
 
@@ -70,21 +86,16 @@ double pulseToCm(long pulseLength) {
   return (double) pulseLength / 58;
 }
 
-double idealPosition(double range) {
-  double dImage = range - LENSPOSITION;
-  double dObject = (double) 1 / FOCALLENGTH - (double) 1 / dImage;
-  
-  double idealPosition = LENSPOSITION - dObject;
-  return idealPosition;
+double idealDObject(double dImage) {
+  return 1.0 / ( 1.0 / FOCALLENGTH - 1.0 / dImage );
 }
 
 int angleForPos(double newPos) {
-  double servoRelativePos = newPos - SERVOPOSITION;
   // https://casry.hackpad.com/Servo-Calculations-15QDaQiFuMh
-  return (int) ((atan(servoRelativePos / TRACKHEIGHT) * 180.0 / PI) + 90);
+  return (int) (90 + (atan(newPos / TRACKHEIGHT) * 180.0 / M_PI));
 }
 
-void moveToPos(int angle) {
+void moveToAngle(int angle) {
   servo.write(angle);
 }
 
